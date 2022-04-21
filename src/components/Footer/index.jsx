d28e1baad0993230e0db4100639257c37582b827
brayanpesantes <brayalexis.30@gmail.com>
2022-04-21 09:38:49 -0500

    terminado el foter

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,45 +14,44 @@ function Footer() {
         justifyContent="space-between"
         alignItems={"flex-start"}
       >
-        <Box>
+        <Box py={4}>
           <Image src='../../../public/images/logo-white.svg' />
         </Box>
         <Stack alignItems={"flex-start"}>
           <Stack direction={["column", 'row']} spacing={8}>
             <VStack spacing={2}>
-              <Heading fontSize={["1.5rem", "2.5rem"]} mb={4}>Features</Heading>
+              <Heading fontSize={["1.5rem", "2rem"]} mb={4}>Features</Heading>
               <Link fontSize={["1rem", "1.2rem"]}>Link Shortening</Link>
               <Link fontSize={["1rem", "1.2rem"]}>Branded Links</Link>
               <Link fontSize={["1rem", "1.2rem"]}>Analytics</Link>
             </VStack>
             <VStack>
-              <Heading fontSize={["1.5rem", "2.5rem"]} mb={4}>Resources</Heading>
+              <Heading fontSize={["1.5rem", "2rem"]} mb={4}>Resources</Heading>
               <Link fontSize={["1rem", "1.2rem"]}>Blog</Link>
               <Link fontSize={["1rem", "1.2rem"]}>Developers</Link>
               <Link fontSize={["1rem", "1.2rem"]}>Support</Link>
             </VStack>
             <VStack>
-              <Heading fontSize={["1.5rem", "2.5rem"]} mb={4}>Company</Heading>
+              <Heading fontSize={["1.5rem", "2rem"]} mb={4}>Company</Heading>
               <Link fontSize={["1rem", "1.2rem"]}>About</Link>
               <Link fontSize={["1rem", "1.2rem"]}>Our Team</Link>
               <Link fontSize={["1rem", "1.2rem"]}>Careers</Link>
             </VStack>
-            <Box py={8}>
-              <HStack spacing={8} >
-                <Link>
-                  <Image src='../../../public/images/icon-facebook.svg' />
-                </Link>
-                <Link>
-                  <Image src='../../../public/images/icon-twitter.svg' />
-                </Link>
-                <Link>
-                  <Image src='../../../public/images/icon-pinterest.svg' />
-                </Link>
-                <Link>
-                  <Image src='../../../public/images/icon-instagram.svg' />
-                </Link>
-              </HStack>
-            </Box>
+
+            <HStack spacing={8} py={4} align={"start"} >
+              <Link>
+                <Image src='../../../public/images/icon-facebook.svg' />
+              </Link>
+              <Link>
+                <Image src='../../../public/images/icon-twitter.svg' />
+              </Link>
+              <Link>
+                <Image src='../../../public/images/icon-pinterest.svg' />
+              </Link>
+              <Link>
+                <Image src='../../../public/images/icon-instagram.svg' />
+              </Link>
+            </HStack>
           </Stack>
         </Stack>
       </HStack>
